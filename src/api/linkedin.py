fb57e31f935736b826ddd45f6b359e4b1c843aac
carlos.rian <carlos.rian@quartile.com>
2024-11-24 12:47:08 -0300

    remove logging of profile URL requests in LinkedIn API

--- src/api/linkedin.py
+++ src/api/linkedin.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Query
 
 from ..const import LINKEDIN_PROFILE_URL_REGEX
-from ..logger import Logger
 from .service.linkedin import LinkedinService
 
 router = APIRouter()
@@ -9,5 +8,4 @@ router = APIRouter()
 
 @router.get("/profile")
 async def get_linkedin_profile(profile_url: str = Query(..., pattern=LINKEDIN_PROFILE_URL_REGEX)):
-    Logger.info(f"Received request for profile URL: {profile_url}")
     return LinkedinService.get_linkedin_profile(profile_url=profile_url)
