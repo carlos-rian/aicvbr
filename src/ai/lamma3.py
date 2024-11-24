73219d2bfd5e0ddd29af03cff1dae0741b0a9f95
carlos.rian <carlos.rian@quartile.com>
2024-11-24 18:25:58 -0300

    Refactor model initialization and add asynchronous message generation for LinkedIn profiles and job vacancies

--- src/ai/lamma3.py
+++ src/ai/lamma3.py
@@ -1,6 +1,26 @@
-from langchain_ollama import ChatOllama, OllamaLLM
+from langchain_ollama import ChatOllama
 
-model = OllamaLLM(model="llama3.2")
+model = ChatOllama(model="llama3.2")
 
-for content in model.stream("Come up with 10 names for a song about parrots"):
-    print(content, end="", flush=True)
+
+async def ollama_message(site_content: str, linkedin_perfil: str) -> str:
+    message = f"""
+LinkedIn Profile: {linkedin_perfil}
+
+Job Vacancy: {site_content}
+
+Based on my linkedin profile and the job vacancy, generate a curriculum for me.
+    """
+
+    messages = [
+        # you are a specialist in Create curriculum for ATS( Applicant Tracking System) and LMS (Learning Management System) platforms
+        {
+            "role": "system",
+            "content": "You are a specialist in treate curriculum for ATS(Applicant Tracking System) and LMS (Learning Management System) platforms",
+        },
+        {"role": "user", "content": message},
+    ]
+
+    stream = await model.ainvoke(messages)
+    async for content in stream:
+        print(content, end="", flush=True)
