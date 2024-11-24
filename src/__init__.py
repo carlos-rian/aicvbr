fafcde318bb8577fbebf069348a20b0aa27d4c3f
carlos.rian <carlos.rian@quartile.com>
2024-11-24 18:25:15 -0300

    Rename main.py to __init__.py and add job API router integration

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from .api.job import router as job_router
 from .api.linkedin import router as linkedin_router
 
 
@@ -7,6 +8,7 @@ def create_app():
     app = FastAPI()
 
     app.include_router(linkedin_router, prefix="/api/linkedin")
+    app.include_router(job_router, prefix="/api/jobs")
     return app
 
 
