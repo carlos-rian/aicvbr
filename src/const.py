a395b4cfb1336acf57b32e298b336cd1ec2f98d4
carlos.rian <carlos.rian@quartile.com>
2024-11-24 18:26:07 -0300

    Use os.getenv for LinkedIn credentials to improve environment variable handling

--- src/const.py
+++ src/const.py
@@ -1,6 +1,6 @@
 import os
 
-LINKEDIN_USERNAME = os.environ["LINKEDIN_USERNAME"]
-LINKEDIN_PASSWORD = os.environ["LINKEDIN_PASSWORD"]
+LINKEDIN_USERNAME = os.getenv("LINKEDIN_USERNAME")
+LINKEDIN_PASSWORD = os.getenv("LINKEDIN_PASSWORD")
 LINKEDIN_PROFILE_URL_REGEX = "https://www.linkedin.com/in/.*"
 LINKEDIN_EXTRACT_PUBLIC_PROFILE = r"https:\/\/www\.linkedin\.com\/in\/([a-zA-Z0-9\-]+)\/?"
