8bc8abee2e27715f1f379b868f02edea83c62885
carlos.rian <carlos.rian@quartile.com>
2024-11-24 18:26:13 -0300

    Implement asynchronous main function for LinkedIn profile content formatting and site content retrieval

--- test.py
+++ test.py
@@ -1,23 +1,10 @@
-import re
-
-# URL without a trailing slash
-url = "https://www.linkedin.com/in/carlos-rian/"
-pattern = r"https:\/\/www\.linkedin\.com\/in\/([a-zA-Z0-9\-]+)\/?"
-match = re.search(pattern, url)
-
-if match:
-    public_id = match.group(1)
-    print(f"Public ID: {public_id}")
-else:
-    print("No match found.")
-
-
-import json
 import logging
 import os
 
+from src.ai.lamma3 import ollama_message
 from src.const import LINKEDIN_PASSWORD, LINKEDIN_USERNAME
 from src.crawler.linkedin import LinkedinCrawler
+from src.crawler.sites import get_site_content
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -25,6 +12,18 @@ PUBLIC_ID = os.environ["LINKEDIN_PUBLIC_ID"]
 
 crawler = LinkedinCrawler(username=LINKEDIN_USERNAME, password=LINKEDIN_PASSWORD)
 profile = crawler.get_profile(public_id=PUBLIC_ID)
-connections = crawler.get_profile_connections(urn_id=profile.urn_id)
 
-print(profile.model_dump_json(indent=2))
+
+async def main():
+    profile_content = crawler.format_as_text()
+    site_content = await get_site_content(
+        url="https://magnasistemas.gupy.io/job/eyJqb2JJZCI6ODA3NTY1MSwic291cmNlIjoiZ3VweV9wb3J0YWwifQ==?jobBoardSource=gupy_portal"
+    )
+
+    await ollama_message(site_content=site_content.content, linkedin_perfil=profile_content)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
