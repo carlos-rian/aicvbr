840cdddd890c8fba5f5e9dc0130d1b4ba53a3f71
carlos.rian <carlos.rian@quartile.com>
2024-11-26 10:26:28 -0300

    Add profile content formatting and save to markdown file; update dependencies

--- test.py
+++ test.py
@@ -12,10 +12,13 @@ PUBLIC_ID = os.environ["LINKEDIN_PUBLIC_ID"]
 
 crawler = LinkedinCrawler(username=LINKEDIN_USERNAME, password=LINKEDIN_PASSWORD)
 profile = crawler.get_profile(public_id=PUBLIC_ID)
+profile_content = crawler._format_as_text()
+
+with open("profile.md", "w") as f:
+    f.write(profile_content)
 
 
 async def main():
-    profile_content = crawler.format_as_text()
     site_content = await get_site_content(
         url="https://gruponexxees.gupy.io/job/eyJqb2JJZCI6ODEzODA1Mywic291cmNlIjoiZ3VweV9wb3J0YWwifQ==?jobBoardSource=gupy_portal"
     )
